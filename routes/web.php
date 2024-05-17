48c0d7c4915c5586c6332e263f445ee6c21d587d
fatazakie <160807919+fatazakie@users.noreply.github.com>
2024-05-17 20:39:51 +0700

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
     
     // Route::get('/mahasiswa/', [MahasiswaController::class, 'index']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

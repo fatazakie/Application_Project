f5057443980a6238d46511c2c380688697a88f02
fatazakie <160807919+fatazakie@users.noreply.github.com>
2024-05-18 17:13:53 +0700

    buat controller barang

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,18 +63,11 @@
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="barang" data-toggle="collapse" data-target="#collapseTwo"
+                <a class="nav-link collapsed" href="/barang/" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fa-solid fa-boxes-packing"></i>
                     <span>Barang</span>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
             </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/barang/', [App\Http\Controllers\HomeController::class, 'index']);
